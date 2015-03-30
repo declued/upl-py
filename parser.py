34d819bd5054d3d4e27ae930a1591ce9ab40f05c
Hadi Moshayedi <hadi@moshayedi.net>
2015-03-31 00:59:26 +0300

    Some bit of clean-up and readability.

--- parser.py
+++ parser.py
@@ -26,6 +26,11 @@ class Parser(object):
         return self.parse_program()
 
     def parse_program(self):
+        """
+        On success returns a program, otherwise returns None.
+
+        program := statement [| program];
+        """
         statements = []
         error = False
 
@@ -49,105 +54,108 @@ class Parser(object):
         On success returns a statement and increments token_index, otherwise
         returns None.
 
-        statement := declaration | expression;
+        statement := declaration ";" | expression ";";
         """
+        statement_tokens = self.get_current_statement()
+
         # a statement is a declaration or an expression
-        statement = self.parse_declaration()
+        statement = self.parse_declaration(statement_tokens)
+        if statement is None:
+            statement = self.parse_expression(statement_tokens)
 
         if statement is None:
             # if we couldn't match a declaration or an expression, it's an error
             self.error = True
             result = None
-        elif self.current_token_type() != TokenType.StatementSep:
-            # if statement delimiter was not found, it's an error
-            self.error = True
-            result = None
         else:
             # all is good
-            self.token_index += 1
+            self.token_index += len(statement_tokens) + 1
             result = statement
 
         return result
 
-    def parse_declaration(self):
+    def parse_declaration(self, tokens):
         """
         On success returns a declaration and increments the token_index, otherwise
         returns None.
 
-        declaration := declarator identifier "=" expression | func_def;
+        declaration := declarator identifier "=" expression;
         declarator := "def" | "var" | typename;
         typename := "bool" | "int" | "real";
         """
+        if len(tokens) < 4:
+            return None
+
         initial_token_index = self.token_index
         declarator = None
         identifier = None
         expression = None
 
         # match the declarator
-        if self.current_token_type() not in (TokenType.KeywordDef,
-                                             TokenType.KeywordVar,
-                                             TokenType.KeywordBool,
-                                             TokenType.KeywordInt,
-                                             TokenType.KeywordReal):
-            self.token_index = initial_token_index
+        if tokens[0].type not in (TokenType.KeywordDef, TokenType.KeywordVar,
+                                  TokenType.KeywordBool, TokenType.KeywordInt,
+                                  TokenType.KeywordReal):
             return None
         else:
-            declarator = self.current_token_type()
-
-        self.token_index += 1
+            declarator = tokens[0].type
 
         # match the identifier
-        if self.current_token_type() != TokenType.Identifier:
-            self.token_index = initial_token_index
+        if tokens[1].type != TokenType.Identifier:
             return None
         else:
-            identifier = self.current_token().value
-
-        self.token_index += 1
+            identifier = tokens[1].value
 
         # match the assignment sign
-        if self.current_token_type() != TokenType.Assignment:
-            self.token_index = initial_token_index
+        if tokens[2].type != TokenType.Assignment:
             return None
 
-        self.token_index += 1
-
-        # match the rhs
-        rhs = self.parse_func_def()
+        # match the right hand side
+        rhs = self.parse_expression(tokens[3:])
         if rhs is None:
-            statement_tokens = self.get_current_statement()
-            rhs = self.parse_expression(statement_tokens)
-            self.token_index += len(statement_tokens)
-
-        if rhs is None:
-            self.token_index = initial_token_index
             return None
 
         declaration = DeclNode(declarator, identifier, rhs)
         return declaration
 
-    def parse_func_def(self):
-        return None
-
     def get_current_statement(self):
+        """
+        Returns the statement which starts at the current token.
+        """
         tokens = []
+        balance = 0
         for token in self.tokens[self.token_index:]:
-            if token.type != TokenType.StatementSep:
-                tokens.append(token)
-            else:
+            if token.type == TokenType.StatementSep and balance == 0:
                 break
+
+            tokens.append(token)
+
+            if token.type == TokenType.OpenBracket:
+                balance += 1
+            elif token.type == TokenType.CloseBracket:
+                balance -= 1
 
         return tokens
 
     def parse_expression(self, tokens):
+        """
+        On success returns an expression, otherwise returns None.
+
+        expression := binary_operation | unary_operation | func_call | func_def | literal;
+        """
         expression = self.parse_binary_operation(tokens) or\
                      self.parse_unary_operation(tokens) or\
                      self.parse_function_call(tokens) or\
+                     self.parse_function_def(tokens) or\
                      self.parse_literal(tokens)
 
         return expression
 
     def parse_binary_operation(self, tokens):
+        """
+        On success returns a binary operation, otherwise returns None.
+
+        binary_operation := expression operator expression;
+        """
         # find highest priority operator
         operator_index = None
         operator_priority = 100
@@ -167,6 +175,7 @@ class Parser(object):
         if operator_index is None:
             return None
 
+        # parse left operand and right operand
         operator = tokens[operator_index].value
         left_operand = self.parse_expression(tokens[:operator_index])
         right_operand = self.parse_expression(tokens[operator_index + 1:])
@@ -174,11 +183,17 @@ class Parser(object):
         if left_operand is None or right_operand is None:
             return None
 
+        # create the result
         binary_operation = BinaryOperationNode(operator, left_operand, right_operand)
 
         return binary_operation
 
     def parse_unary_operation(self, tokens):
+        """
+        On success returns a unary operation, otherwise returns None.
+
+        unary_operation := operator expression;
+        """
         if len(tokens) == 0 or tokens[0].type != TokenType.Operator:
             return None
 
@@ -195,7 +210,15 @@ class Parser(object):
     def parse_function_call(self, tokens):
         return None
 
+    def parse_function_def(self, tokens):
+        return None
+
     def parse_literal(self, tokens):
+        """
+        On success returns a literal, otherwise returns None.
+
+        literal := bool_literal | int_literal | real_literal;
+        """
         if len(tokens) != 1:
             return None
 
@@ -215,26 +238,8 @@ class Parser(object):
     def get_operator_priority(self, operator):
         return 1
 
-    def current_token(self):
-        """
-        If a token is available, returns it, otherwise retursn None.
-        """
-        if self.token_index >= len(self.tokens):
-            return None
-        else:
-            return self.tokens[self.token_index]
-
-    def current_token_type(self):
-        """
-        If a token is available, returns its type, otherwise returns None.
-        """
-        if self.current_token() is None:
-            return None
-        else:
-            return self.current_token().type
-
 if __name__ == "__main__":
-    program = "var a = -1+5;int x = 10;bool v=true;"
+    program = "var a = -1+5;int x = 10;bool v=true;1+3;"
 
     tokens = tokenize_program(program)
     parse_tree = Parser(tokens).parse()
