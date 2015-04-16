e44609581c5879dad76423d41c0a6ba48da0cfa0
Hadi Moshayedi <hadi@moshayedi.net>
2015-04-16 05:49:00 +0300

    Some more semantic analyzer tests.

--- tests/semantic_analyzer_tests.py
+++ tests/semantic_analyzer_tests.py
@@ -8,7 +8,7 @@ TYPE_INT = "BasicType.Int"
 class TestSemanticAnalyzer(UPLTestCase):
     def test_constant_function(self):
         self.checkSemanticTree("""
-        def f = () -> int { 2; }
+            def f = () -> int { 2; }
         """, [{
             "type": "FuncDefAnalyzeNode",
             "name": "f",
@@ -18,6 +18,50 @@ class TestSemanticAnalyzer(UPLTestCase):
                 "type": "ConstantAnalyzeNode"
             }
         }])
+
+    def test_reference_to_constant(self):
+        self.checkSemanticTree("""
+            def some_constant = 10;
+            def f = () -> int
+            {
+                some_constant;
+            }
+        """, [{
+            "body": {
+                "type": "ConstantAnalyzeNode"
+            }
+        }])
+
+    def test_reference_to_arg(self):
+        self.checkSemanticTree("""
+            def f = (a: int) -> int
+            {
+                a;
+            }
+        """, [{
+            "body": {
+                "type": "FuncArgAnalyzeNode"
+            }
+        }])
+
+    def test_call_to_another_func(self):
+        self.checkSemanticTree("""
+            def f = () -> int { 1; };
+            def g = () -> int { f(); };
+        """, [
+            {"body": {"type": "ConstantAnalyzeNode"}},
+            {"body": {"type": "FuncCallAnalyzeNode"}}
+        ])
+
+    def test_multi_statement_body(self):
+        self.checkSemanticTree("""
+            def f = () -> int {
+                def a = 1;
+                a;
+            };
+        """, [
+            {"body": {"type": "ConstantAnalyzeNode"}}
+        ])
 
     def checkSemanticTree(self, program, partial_semantic_tree):
         tokens = lexer.tokenize_program(program)
